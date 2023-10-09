17db971c4175857604af778ad575aec884ad9b8e
yennhi442002 <20521721@gm.uit.edu.vn>
2023-10-09 13:28:46 +0700

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Logo from "../assets/pizzaLogo.png";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 
@@ -9,22 +8,23 @@ function Navbar() {
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
   };
+
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+        <h1 className="h1"> Pizza cua Khang</h1>
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/about"> About </Link>
           <Link to="/contact"> Contact </Link>
         </div>
+        
       </div>
       <div className="rightSide">
         <Link to="/"> Home </Link>
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
-        <button onClick={toggleNavbar}>
-        </button>
+        <button onClick={toggleNavbar}>Toggle Navbar</button>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -6,11 +6,31 @@ import BannerImage1 from "../assets/a.jpg";
 import BannerImage2 from "../assets/b.jpg";
 import BannerImage3 from "../assets/c.jpg";
 import "../styles/Home.css";
-
 function Home() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  // Function to toggle the dropdown menu
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div className="home">
-      <h1 className="commonText"> Pizza cua Khang </h1>
+      {/* Add a button to toggle the dropdown */}
+      <button className="dropdown-button" onClick={toggleDropdown}>
+        Pizza Categories
+      </button>
+
+      {/* Render the dropdown menu based on the state */}
+      {isDropdownOpen && (
+        <div className="dropdown-content">
+          {/* Dropdown content goes here */}
+          <a href="#">Item 1</a>
+          <a href="#">Item 2</a>
+          <a href="#">Item 3</a>
+        </div>
+      )}
+
       <Carousel
         showArrows={true}
         infiniteLoop={true}
@@ -37,5 +57,6 @@ function Home() {
     </div>
   );
 }
+
 
 export default Home;
